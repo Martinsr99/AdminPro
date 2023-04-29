3c028909d790a605c6789d74a84fc7dc7b91b6a5
Martinsr99 <silesreche.martin@gmail.com>
2023-04-29 17:28:29 +0200

    feat: Add register and notFund 404 pages

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
-  templateUrl: './register.component.html'
+  styleUrls: ['./register.component.css'],
+  templateUrl: './register.component.html',
 })
 export class RegisterComponent {
 
--- src/app/pages/nopagefound/nopagefound.component.ts
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nopagefound',
-  templateUrl: './nopagefound.component.html'
+  templateUrl: './nopagefound.component.html',
+  styleUrls: ['./nopagefound.component.css'],
 })
 export class NopagefoundComponent {
-
+  year = new Date().getFullYear()
 }
