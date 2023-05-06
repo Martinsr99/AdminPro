129e9c6e2ebfe0897be4b3e4331be733571b2746
Martinsr99 <silesreche.martin@gmail.com>
2023-05-06 13:36:32 +0200

    feat: Create sidebar service and manage logout

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+declare function customInitFunctions()
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html'
@@ -10,8 +11,9 @@ export class PagesComponent {
   constructor(private settingsService: SettingsService){}
 
   ngOnInit():void{
-
+    customInitFunctions()
 
   }
 
 }
+
