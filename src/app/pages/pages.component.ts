2a8d985452a2ef2a74b537ae2d294be9833ad97c
Martinsr99 <silesreche.martin@gmail.com>
2023-05-07 13:10:46 +0200

    feat: Add some code to practice with rxjs

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
-declare function customInitFunctions()
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html'
@@ -9,11 +8,5 @@ declare function customInitFunctions()
 export class PagesComponent {
 
   constructor(private settingsService: SettingsService){}
-
-  ngOnInit():void{
-    customInitFunctions()
-
-  }
-
 }
 
--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, Subscription, interval, map, take } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html'
 })
-export class RxjsComponent {
+export class RxjsComponent implements OnDestroy{
+
+  public intervalSubs: Subscription;
+
+  constructor() {
+
+    this.intervalSubs = this.retornaIntervalo().subscribe(console.log);
+
+  }
+
+  retornaObservable() {
+    const obs$ = new Observable(observer => {
+      let i=0
+      const intervalo = setInterval(() => {
+        i++
+        observer.next(i)
+
+        if(i===4){
+          clearInterval(intervalo)
+          observer.complete()
+        }
+      },1000)
+    });
+
+    obs$.subscribe(valor => console.log(valor))
+  }
+
+  retornaIntervalo() {
+    const intervalo$ = interval(1000).pipe(take(4),map(valor => valor+1))
+
+    return intervalo$
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubs.unsubscribe()
+  }
 
 }
