95a20983e1fb26bf6170923415b4fb377f64ffdf
Martinsr99 <silesreche.martin@gmail.com>
2023-05-17 18:09:58 +0200

    feat: Add logic for token in loginPage

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,16 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 
+declare const google: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
 
-  constructor(private router: Router){}
+  @ViewChild('googleBtn') googleBtn: ElementRef;
+
+  public formSubmitted = false;
+
+  loginForm: FormGroup; //Agregar el loginForm en un FormGroup, e importar desde: import { FormGroup } from '@angular/forms';
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService
+  ) {
+    //Esta parte moverlo dentro de los corchetes del constructor
+    this.loginForm = this.fb.group({
+      email: [
+        localStorage.getItem('email') || '',
+        [Validators.required, Validators.email],
+      ],
+      password: ['', Validators.required],
+      remember: [false],
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.googleInit();
+  }
+
+  googleInit() {
+    google.accounts.id.initialize({
+      client_id:
+        '556660919037-og65tufivptiheli7nl7lvgi8pd4ip8c.apps.googleusercontent.com',
+      callback:(response:any) => this.handleCredentialResponse(response),
+    });
+    google.accounts.id.renderButton(
+      this.googleBtn.nativeElement,
+      { theme: 'outline', size: 'large' } // customization attributes
+    );
+  }
+
+  handleCredentialResponse(response: any) {
+    console.log('Encoded JWT ID token: ' + response.credential);
+    this.usuarioService.loginGoogle(response.credential).subscribe(resp => {
+      console.log({login:resp})
+      this.router.navigateByUrl('/')
+    })
+  }
 
   login() {
-    this.router.navigateByUrl('/');
+    this.usuarioService.login(this.loginForm.value).subscribe(
+      (resp) => {
+        if (this.loginForm.get('remember').value) {
+          localStorage.setItem('email', this.loginForm.get('email').value);
+        } else {
+          localStorage.removeItem('email');
+        }
+        console.log(resp);
+      },
+      (err) => {
+        Swal.fire('Error', err.error.error, 'error');
+      }
+    );
   }
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {tap} from 'rxjs/operators'
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { environment } from '../environments/environment';
+import { LoginForm } from '../interfaces/login-form.interfaces';
 
 const base_url = environment.base_url
 
@@ -13,6 +15,20 @@ export class UsuarioService {
   constructor(private http:HttpClient) { }
 
   crearUsuario(formData:RegisterForm){
-    return this.http.post(`${base_url}/usuarios`,formData)
+    return this.http.post(`${base_url}/usuarios`,formData).pipe(tap((resp:any) => {
+      localStorage.setItem('token', resp.token)
+    }))
+  }
+  login(formData:LoginForm){
+    return this.http.post(`${base_url}/login`,formData).pipe(tap((resp:any) => {
+      localStorage.setItem('token', resp.token)
+    }))
+  }
+
+  loginGoogle(token:string){
+    return this.http.post(`${base_url}/login/google`,{token}).pipe(tap((resp:any) => {
+      localStorage.setItem('token',resp.token),
+      console.log(resp)
+    }))
   }
 }
