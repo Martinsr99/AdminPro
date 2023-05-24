4c434e1f546fa72bea465a1eccf23ba63a24fd1b
Martinsr99 <silesreche.martin@gmail.com>
2023-05-24 12:10:28 +0200

    feat: Add logic for hospitals

--- src/app/pages/mantenimientos/hospitales/hospitales.component.ts
+++ src/app/pages/mantenimientos/hospitales/hospitales.component.ts
@@ -1,31 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription, delay } from 'rxjs';
 import { Hospital } from 'src/app/models/hospital.model';
 import { HospitalService } from 'src/app/services/hospital.service';
+import { ModalImagenService } from 'src/app/services/modal-imagen.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-hospitales',
-  templateUrl: './hospitales.component.html'
+  templateUrl: './hospitales.component.html',
 })
 export class HospitalesComponent implements OnInit {
+  public hospitales: Hospital[] = [];
+  public cargando: boolean = true;
+  public imgSubs: Subscription;
 
-  public hospitales: Hospital[] = []
-  public cargando: boolean = true
 
-  constructor(private hospitalService:HospitalService){
+  constructor(
+    private hospitalService: HospitalService,
+    private modalImagenService: ModalImagenService
+  ) {}
 
+  ngOnInit() {
+    this.cargarHospitales();
+    this.imgSubs = this.modalImagenService.nuevaImagen.pipe(delay(500)).subscribe(img => this.cargarHospitales())
   }
 
-  ngOnInit(){
-    this.cargarHospitales()
-  }
-  
-  cargarHospitales(){
-    
-    this.cargando = true
-    this.hospitalService.cargarHospitales().subscribe(hospitales => {
-      this.cargando = false
-      this.hospitales = hospitales
-    })
+  cargarHospitales() {
+    this.cargando = true;
+    this.hospitalService.cargarHospitales().subscribe((hospitales) => {
+      this.cargando = false;
+      this.hospitales = hospitales;
+    });
   }
 
+  eliminarHospital(hospital: Hospital) {
+    this.hospitalService.borrarHospital(hospital._id).subscribe((resp) => {
+      this.cargarHospitales();
+      Swal.fire('Borrado', hospital.nombre, 'success');
+    });
+  }
+
+  guardarCambios(hospital: Hospital) {
+    this.hospitalService
+      .actualizarHospital(hospital._id, hospital.nombre)
+      .subscribe((resp) => {
+        Swal.fire('Actualizado', hospital.nombre, 'success');
+      });
+  }
+
+  async abrirSweetAlert() {
+    const { value: nombre } = await Swal.fire<string>({
+      title: 'Crear hospital',
+      text: 'Ingrese el nombre del nuevo hospital',
+      input: 'text',
+      inputPlaceholder: 'Nombre del hospital',
+      showCancelButton: true,
+    });
+    if (nombre.trim().length > 0) {
+      this.hospitalService.crearHospitales(nombre).subscribe((resp: any) => {
+        this.hospitales.push(resp.hospital);
+      });
+    }
+  }
+
+  abrirModal(hospital: Hospital) {
+    this.modalImagenService.abrirModal(
+      'hospitales',
+      hospital._id,
+      hospital.img
+    );
+  }
 }
