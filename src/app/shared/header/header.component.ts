d43dc7efd7fa98af65ff47763306b9aa891b4fb3
Martinsr99 <silesreche.martin@gmail.com>
2023-05-27 12:47:12 +0200

    feat: Add first feature for global search

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,21 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Usuario } from 'src/app/models/usuario.model';
 import { UsuarioService } from 'src/app/services/usuario.service';
-
+import Swal from 'sweetalert2';
+ 
+declare let $: any; //*****IMPORTANTE DECLARAR variable $
+ 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styles: [
+  ]
 })
-export class HeaderComponent {
-
-  public usuario:Usuario
-
-  constructor(private usuarioService: UsuarioService){
-    this.usuario = usuarioService.usuario
+export class HeaderComponent implements OnInit, AfterViewInit {
+ 
+ 
+  public usuario: Usuario;
+ 
+  constructor(private usuarioService: UsuarioService,
+    private router: Router) {
+    this.usuario = usuarioService.usuario;
   }
-
-  logout(){
+ 
+  ngOnInit(): void {
+ 
+  }
+ 
+  ngAfterViewInit(): void {
+    //NOTA: esta funcion esta en el archivo "custom.js"
+    $(".search-box a, .search-box .app-search .srh-btn").on('click', function () {
+      $(".app-search").toggle(200);
+    });
+  }
+ 
+  buscar(strBusqueda: string) {
+    console.log(strBusqueda);
+  }
+ 
+  logout() {
     this.usuarioService.logout();
+    this.router.navigateByUrl('/login');
   }
-
 }
